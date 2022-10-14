dd61aee38943ce4344919675af988a015f31c98f
Ahmed Tarek <ahmed.tarek55525@gmail.com>
2022-10-14 20:56:06 +0200

    SPI Driver

---
+++ MCAL/SPI/spi.c
@@ -0,0 +1,89 @@
+/******************************************************************************
+*  File name:		main.c
+*  Author:			Oct 14, 2022
+*  Author:			Ahmed Tarek
+*******************************************************************************/
+
+/*******************************************************************************
+*                        		Inclusions                                     *
+*******************************************************************************/
+
+#include "spi.h"
+#include "avr/io.h"
+#include "../GPIO/gpio.h"
+#include "../../LIB/common_macros.h"
+
+/*******************************************************************************
+*                           Global Variables                                  *
+*******************************************************************************/
+
+/* to store the special char to end sting at and it has default value = # */
+static uint8 g_endStringChar = '#';
+
+/*******************************************************************************
+*                      Functions Definitions                                   *
+*******************************************************************************/
+
+void SPI_initMaster(void)
+{
+	GPIO_setupPinDirection(PORTB_ID, PIN4_ID, PIN_OUTPUT);
+	GPIO_setupPinDirection(PORTB_ID, PIN5_ID, PIN_OUTPUT);
+	GPIO_setupPinDirection(PORTB_ID, PIN6_ID, PIN_INPUT);
+	GPIO_setupPinDirection(PORTB_ID, PIN7_ID, PIN_OUTPUT);
+
+	SPCR = (1<<SPE) | (1<<MSTR);
+
+	SPSR &= ~(1<<SPI2X);
+}
+
+void SPI_initSlave(void)
+{
+	GPIO_setupPinDirection(PORTB_ID, PIN4_ID, PIN_INPUT);
+	GPIO_setupPinDirection(PORTB_ID, PIN5_ID, PIN_INPUT);
+	GPIO_setupPinDirection(PORTB_ID, PIN6_ID, PIN_OUTPUT);
+	GPIO_setupPinDirection(PORTB_ID, PIN7_ID, PIN_INPUT);
+
+	SPCR = (1<<SPE) ;
+
+	SPSR &= ~(1<<SPI2X);
+}
+
+uint8 SPI_sendReceive(uint8 data)
+{
+	SPDR = data;
+
+	while(BIT_IS_CLEAR(SPSR,SPIF)){}
+
+	return SPDR;
+}
+
+void SPI_sendString(const uint8 *str)
+{
+	uint8 i = 0;
+	uint8 received_data = 0;
+	while(str[i] != '\0')
+	{
+		received_data = SPI_sendString(str[i]);
+		i++;
+	}
+	str[i] = '\0';
+}
+
+void SPI_receiveString(uint8 *str)
+{
+	uint8 i = 0;
+	/* as 0xFF considered as garbage value is sent to just receive the string */
+	str[i] = SPI_sendReceiveByte(SPI_DEFAULT_DATA_VALUE);
+
+	while(str[i] != g_endStringChar)
+	{
+		i++;
+		str[i] = SPI_sendReceiveByte(SPI_DEFAULT_DATA_VALUE);
+	}
+	str[i] = '\0' ;
+}
+
+void SPI_setEndStringChar(uint8 symbol)
+{
+	g_endStringChar = symbol ;
+}
---
+++ MCAL/SPI/spi.h
@@ -0,0 +1,88 @@
+/******************************************************************************
+*  File name:		main.c
+*  Author:			Oct 14, 2022
+*  Author:			Ahmed Tarek
+*******************************************************************************/
+
+#ifndef MCAL_SPI_SPI_H_
+#define MCAL_SPI_SPI_H_
+
+/*******************************************************************************
+*                        		Inclusions                                     *
+*******************************************************************************/
+
+#include "../../LIB/std_types.h"
+
+/*******************************************************************************
+*                        		Definitions                                    *
+*******************************************************************************/
+
+#define SPI_DEFAULT_DATA_VALUE			0xFF
+
+
+/*******************************************************************************
+*                      Functions Prototypes                                   *
+*******************************************************************************/
+
+/*******************************************************************************
+* Function Name:		SPI_initMaster
+* Description:			Initialize the SPI device as Master.
+* Parameters (in):    	None
+* Parameters (out):   	None
+* Return value:      	void
+********************************************************************************/
+
+void SPI_initMaster(void);
+
+/*******************************************************************************
+* Function Name:		SPI_initSlave
+* Description:			Initialize the SPI device as Slave.
+* Parameters (in):    	None
+* Parameters (out):   	None
+* Return value:      	void
+********************************************************************************/
+
+void SPI_initSlave(void);
+
+/*******************************************************************************
+* Function Name:		SPI_sendReceive
+* Description:			Send the required data through SPI to the other SPI device.
+*						In the same time data will be received from the other device.
+* Parameters (in):    	data to be sent or it may be just a garbage so we can receive
+* Parameters (out):   	None
+* Return value:      	uint8
+********************************************************************************/
+
+uint8 SPI_sendReceive(uint8 data);
+
+/*******************************************************************************
+* Function Name:		SPI_sendString
+* Description:			Send the required string through SPI to the other SPI device.
+* Parameters (in):    	String to send it
+* Parameters (out):   	None
+* Return value:      	void
+********************************************************************************/
+
+void SPI_sendString(const uint8 *str);
+
+/*******************************************************************************
+* Function Name:		SPI_receiveString
+* Description:			 Receive the required string SPI from the other SPI device.
+* Parameters (in):    	String to save the received string in it
+* Parameters (out):   	None
+* Return value:      	void
+********************************************************************************/
+
+void SPI_receiveString(uint8 *str);
+
+/*******************************************************************************
+* Function Name:		SPI_setEndStringChar
+* Description:			Function to set the end string special character
+* Parameters (in):    	Symbol to set the end string special char to,it is = # by default
+* Parameters (out):   	None
+* Return value:      	void
+********************************************************************************/
+
+void SPI_setEndStringChar(uint8 symbol);
+
+#endif /* MCAL_SPI_SPI_H_ */
