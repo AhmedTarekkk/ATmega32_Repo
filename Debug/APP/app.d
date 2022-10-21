8ed098ba3637c00f029403c48ac913806a2dc4ad
Ahmed Tarek <ahmed.tarek55525@gmail.com>
2022-10-21 10:04:23 +0200

    Remove Debug

--- Debug/APP/app.d
+++
@@ -1,3 +0,0 @@
-APP/app.o APP/app.o: ../APP/app.c ../APP/app.h
-
-../APP/app.h:
--- Debug/MCAL/GLI/gli.d
+++
@@ -1,9 +0,0 @@
-MCAL/GLI/gli.o MCAL/GLI/gli.o: ../MCAL/GLI/gli.c \
-  ../MCAL/GLI/../../LIB/common_macros.h ../MCAL/GLI/gli.h \
-  ../MCAL/GLI/../../LIB/std_types.h
-
-../MCAL/GLI/../../LIB/common_macros.h:
-
-../MCAL/GLI/gli.h:
-
-../MCAL/GLI/../../LIB/std_types.h:
