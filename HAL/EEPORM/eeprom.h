7ebcddb152ca5f4a9e71ff62fb661df4bf340f67
Ahmed Tarek <ahmed.tarek55525@gmail.com>
2022-10-22 21:28:03 +0200

    I2C and EEPROM comments

--- HAL/EEPORM/eeprom.h
+++ HAL/EEPORM/eeprom.h
@@ -7,12 +7,41 @@
 #ifndef HAL_EEPORM_EEPROM_H_
 #define HAL_EEPORM_EEPROM_H_
 
+/*******************************************************************************
+*                        		Inclusions                                     *
+*******************************************************************************/
+
 #include "../../LIB/std_types.h"
+
+/*******************************************************************************
+*                        		Definitions                                    *
+*******************************************************************************/
 
 #define ERROR 0
 #define SUCCESS 1
 
+/*******************************************************************************
+*                      Functions Prototypes                                   *
+*******************************************************************************/
+
+/*******************************************************************************
+* Function Name:		EEPROM_writeBytr
+* Description:			Function to initialize the I2C.
+* Parameters (in):    	Required address and the data.
+* Parameters (out):   	SUCCESS or ERROR
+* Return value:      	uint8
+********************************************************************************/
+
 uint8 EEPROM_writeBytr(uint16 address,uint8 byte);
+
+/*******************************************************************************
+* Function Name:		EEPROM_readByte
+* Description:			Function to write byte in specific location.
+* Parameters (in):    	Required address and variable to store the data in it.
+* Parameters (out):   	SUCCESS or ERROR
+* Return value:      	uint8
+********************************************************************************/
+
 uint8 EEPROM_readByte(uint16 address,uint8 *value);
 
 #endif /* HAL_EEPORM_EEPROM_H_ */
